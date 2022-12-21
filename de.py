90580638d3b03599b86b7b7bd8c77cd89fa5d893
JuseTiZ <116796340+JuseTiZ@users.noreply.github.com>
2022-12-21 15:43:20 +0800

    Update de.py

--- de.py
+++ de.py
@@ -9,13 +9,13 @@ for line in raw_file:
 	if string[0] == 'sampleA':
 		continue
 	if len(string) == 11:
-		if abs(float(string[6])) >= 2.0 and float(string[-1]) <= 0.05:
+		if abs(float(string[6])) >= 1.0 and float(string[-1]) <= 0.05:
 			if float(string[6]) > 0:
 				out_up_file.write(string[0] + '\n')
 			else:
 				out_down_file.write(string[0] + '\n')
 	else:
-		if abs(float(string[3])) >= 2.0 and float(string[-1]) <= 0.05:
+		if abs(float(string[3])) >= 1.0 and float(string[-1]) <= 0.05:
 			if float(string[3]) > 0:
 				out_up_file.write(string[0] + '\n')
 			else:
