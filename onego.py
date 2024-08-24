716af0e875f792eb3735ade2128ca0f50078a926
JuseTiZ <291195857@qq.com>
2024-08-24 19:54:46 +0800

    Update script

--- onego.py
+++ onego.py
@@ -1,9 +1,24 @@
-import sys
-raw_file = open(sys.argv[1], "r")
-out_file = open(sys.argv[2], "w")
+import argparse
 
-for line in raw_file:
-	string = line.split("\t")
-	for goid in string[1].strip().split(","):
-		output = string[0] + '\t' + goid + '\n'
-		out_file.write(output)
+def process_go_annotations(raw_file, out_file):
+    with open(raw_file, "r") as infile, open(out_file, "w") as outfile:
+        for line in infile:
+            string = line.split("\t")
+            gene = string[0]
+            go_terms = string[1].strip().split(",")
+            for goid in go_terms:
+                output = gene + '\t' + goid + '\n'
+                outfile.write(output)
+
+def main():
+    parser = argparse.ArgumentParser(description="Process a Trinotate output GO annotation file to create a one-to-one relationship between genes and GO terms.")
+    
+    parser.add_argument('-r', '--raw_file', type=str, help='Path to the Trinotate output raw GO annotation file.')
+    parser.add_argument('-o', '--out_file', type=str, help='Path to the output file where each gene and GO term will have a one-to-one relationship.')
+    
+    args = parser.parse_args()
+    
+    process_go_annotations(args.raw_file, args.out_file)
+
+if __name__ == "__main__":
+    main()
